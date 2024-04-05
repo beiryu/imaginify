2cc42dce1e8f84456f24bf4079758f4fb485ea82
Beiryu <dinhnguyenkhanh210401@gmail.com>
2024-04-05 11:41:13 +0700

    init

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans, Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const ibmPlexSans = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Imaginify",
+  description: "AI-powered image generator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn("font-IBMPlex antialiased", ibmPlexSans.variable)}>{children}</body>
+    </html>
+  );
+}
